1583fb5f1c77b21c94efc79d2900491454a09928
Thiago <thiagobg1811@gmail.com>
2026-08-14 19:22:40 -0300

    first commit

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    private Material _mat;
+    [SerializeField] private float _scrollSpeed = 0.1f;
+
+    void Awake()
+    {
+        _mat = GetComponent<Renderer>().material;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = _mat.mainTextureOffset;
+
+        offset.x += _scrollSpeed * Time.deltaTime;
+
+        _mat.mainTextureOffset = offset;
+    }
+}
